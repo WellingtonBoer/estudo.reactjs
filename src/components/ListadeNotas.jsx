8de49d7cf1aad78e53257c65f6a1ab8c904ec8f2
Wellington Boer <wellington.boer@sitemercado.com.br>
2021-09-01 12:05:10 -0300

    Criacao do projeto

---
+++ src/components/ListadeNotas.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+export default class ListaDeNotas extends Component {
+    render() {
+        return (
+            <ul>
+                <li>
+                    <section>
+                        <header>
+                            Título
+                        </header>
+                        <p>
+                            Escreva sua nota.
+                        </p>
+                    </section>
+                </li>
+                <li>
+                    <section>
+                        <header>
+                            Título
+                        </header>
+                        <p>
+                            Escreva sua nota.
+                        </p>
+                    </section>
+                </li>
+            </ul>
+        )
+    }
+}
